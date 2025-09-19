be1a39c22bf40edbaa0ec247c909299c8696c1b5
HandQ2212 <hn235930@gmail.com>
2025-09-19 15:33:17 +0700

    feat: initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.proptit.todohive"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.proptit.todohive"
-        minSdk = 34
-        targetSdk = 36
+        minSdk = 24
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
     }
 }
 
